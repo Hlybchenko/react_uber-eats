12f208650eadbe0eec9a268667cd057acd3ebdac
OleksandrHlybchenko <logista@meta.ua>
2019-08-20 20:23:18 +0300

    Updates

---
+++ precache-manifest.89536d86cf81bca2f1a857ea3433226d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27fce2238d175886cfa894fab7e64a70",
+    "url": "/react_uber-eats/index.html"
+  },
+  {
+    "revision": "146dcfec48330559a267",
+    "url": "/react_uber-eats/static/css/main.52474ee9.chunk.css"
+  },
+  {
+    "revision": "4e0e47c3d37aa84d63a2",
+    "url": "/react_uber-eats/static/js/2.293594df.chunk.js"
+  },
+  {
+    "revision": "146dcfec48330559a267",
+    "url": "/react_uber-eats/static/js/main.f5d1af8f.chunk.js"
+  },
+  {
+    "revision": "183d5977488a46d51899",
+    "url": "/react_uber-eats/static/js/runtime~main.bef14170.js"
+  }
+]);
